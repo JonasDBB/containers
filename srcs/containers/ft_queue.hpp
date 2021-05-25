76998356187ca30959ca9d054fdc0708bfe67f7c
jbennink <jbennink@student.codam.nl>
2021-05-25 14:05:25 +0200

    queue

---
+++ srcs/containers/ft_queue.hpp
@@ -0,0 +1,60 @@
+#ifndef FT_QUEUE_HPP
+# define FT_QUEUE_HPP
+# include <deque>
+
+namespace ft
+{
+	template<class T, class Container = std::deque<T> >
+	class queue {
+	public:
+		typedef T value_type;
+		typedef Container container_type;
+		typedef size_t size_type;
+
+	private:
+		container_type _c;
+
+	public:
+		explicit queue(const container_type &ctnr = container_type()) : _c(ctnr)
+		{}
+
+		bool empty() const					{ return (this->_c.empty()); }
+
+		size_type	size() const			{ return (this->_c.size()); }
+
+		value_type	&front()				{ return (this->_c.front()); }
+
+		const value_type &front() const		{ return (this->_c.front()); }
+
+		value_type	&back()					{ return (this->_c.back()); }
+
+		const value_type &back() const		{ return (this->_c.back()); }
+
+		void push(const value_type &val)	{ this->_c.push_back(val); }
+
+		void pop()							{ this->_c.pop_front(); }
+
+		friend bool	operator==(const queue<T, Container>& lhs, const queue<T, Container>& rhs)	{ return (lhs._c == rhs._c); }
+		friend bool operator<(const queue<T, Container>& lhs, const queue<T, Container>&  rhs)	{ return (lhs._c < rhs._c); }
+	};
+
+//	template<class T, class Container>
+//	bool	operator==(const queue<T, Container>& lhs, const queue<T, Container>& rhs)	{ return (lhs._c == rhs._c); }
+
+	template <class T, class Container>
+	bool operator!=(const queue<T, Container>& lhs, const queue<T, Container>& rhs)		{ return !(lhs == rhs); }
+
+//	template <class T, class Container>
+//	bool operator<(const queue<T, Container>& lhs, const queue<T, Container>& rhs)		{ return (lhs._c < rhs._c); }
+
+	template <class T, class Container>
+	bool operator<=(const queue<T, Container>& lhs, const queue<T, Container>& rhs)		{ return !(rhs < lhs); }
+
+	template <class T, class Container>
+	bool operator>(const queue<T, Container>& lhs, const queue<T, Container>& rhs)		{ return (rhs < lhs); }
+
+	template <class T, class Container>
+	bool operator>=(const queue<T, Container>& lhs, const queue<T, Container>& rhs)		{ return !(lhs < rhs); }
+}
+
+#endif
